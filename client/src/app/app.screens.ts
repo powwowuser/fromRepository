70ddeb066d80e4543613e6ce0e0ee0f81f532f9a
PowWow Admin <admin@studio.dev.powwowinc.net>
2019-08-02 11:11:15 +0000

    [Studio] Add screen "MainScreen"

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,8 +1,14 @@
 /***  Generated file, do not change.  */
 import { MenuComponent } from './menu/menu';
+import { mainscreen_PhonePortrait } from '../pages/mainscreen/PhonePortrait/mainscreen';
 export class Screens {
   static declarations = [
-    MenuComponent
+    MenuComponent,
+    mainscreen_PhonePortrait
   ];
-  static mapping = {}
+  static mapping = {
+    'mainscreen': {
+      PhonePortrait: mainscreen_PhonePortrait
+    }
+  }
 }
--- client/src/mocks/index.ts
+++ client/src/mocks/index.ts
@@ -1,1 +1,2 @@
+export { mock_mainscreen as mainscreen } from './mainscreen/index';
 export { mock_menu as menu } from './menu/index';
