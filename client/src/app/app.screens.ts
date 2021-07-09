f968bcfd0cad08ddbfaf55733f2acb71445d5b9a
SmartUX Admin <admin@smartux.com>
2021-07-09 10:17:16 +0000

    [Studio] Update settings of application

--- client/src/app/app.screens.ts
+++ client/src/app/app.screens.ts
@@ -1,14 +1,17 @@
 /***  Generated file, do not change.  */
 import { MenuComponent } from './menu/menu';
 import { mainscreen_PhonePortrait } from '../pages/mainscreen/PhonePortrait/mainscreen';
+import { mainscreen_Desktop } from '../pages/mainscreen/Desktop/mainscreen';
 export class Screens {
   static declarations = [
     MenuComponent,
-    mainscreen_PhonePortrait
+    mainscreen_PhonePortrait,
+    mainscreen_Desktop
   ];
   static mapping = {
     'mainscreen': {
-      PhonePortrait: mainscreen_PhonePortrait
+      PhonePortrait: mainscreen_PhonePortrait,
+      Desktop: mainscreen_Desktop
     }
   }
 }
